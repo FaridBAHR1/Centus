db5e11cbf8b66fdb16b84801079034d1a7f676b7
FaridBAHR1 <f4ridb4hri@gmail.com>
2023-05-12 13:56:45 +1100

    Centus - Model View Control

--- js/site.js
+++ js/site.js
@@ -1,4 +1,29 @@
-//Call function
-function Application_Main(){
-    alert("Main Function");
+//get values from interface - controller/start
+function getValues(){
+    //get value from page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    //call generateNumbers
+    let numbers = generateNumbers(startValue, endValue);
+
+    //call displayNumbers
+    ;
+}
+
+//generate numbers from start to endvalue - model/logic
+function generateNumbers(startValue, endValue){
+    let numbers = [];
+    
+    //get all numbers from start to end
+    for (let index = startValue; index <= endValue; index++) {
+        //execute loop until index = endValue
+        numbers.push(index);
+    }
+    return numbers;
+}
+
+//display bold numbers - view/display
+function displayNumbers(){
+    ;
 }
